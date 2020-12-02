a5a0d448e76b6ed0d3b5760e0ce14725a631a4d1
RuliSlim <ruli0to8@gmail.com>
2020-12-02 13:21:55 +0700

    dependency effect in trees

--- src/src/pages/trees.tsx
+++ src/src/pages/trees.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps ; */
 import React, { useState } from "react";
 import { ReactD3TreeItem } from "react-d3-tree";
 import { Box, Button, Grid, Paper, Typography } from "@material-ui/core";
